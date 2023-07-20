c22a37f61a4ff7c1050f201508b6ac194bdf37f5
Garg-Ishita <115682492+Garg-Ishita@users.noreply.github.com>
2023-07-20 11:05:17 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = ({footer}) => {
     <>
     <div className='PreFooter'>
     <div>
-   <img src="https://cdn-icons-png.flaticon.com/128/1594/1594900.png"/>
+   <img src="https://cdn-icons-png.flaticon.com/128/1594/1594900.png" alt="a"/>
     <p><b>Hassle-free replacement</b>
     <br/>
     10-day easy return on mi.com</p>
     </div>
-    <div><img src="https://cdn-icons-png.flaticon.com/128/2563/2563814.png"/> <p> <b>100% secure payments</b> <br/> We support Cards, Wallets, EMI and COD</p></div>
-    <div> <img src="https://cdn-icons-gif.flaticon.com/6172/6172532.gif"/> <p> <b>Vast service network</b> <br/> 1000 Mi service-centers across 600 cities</p></div>
+    <div><img src="https://cdn-icons-png.flaticon.com/128/2563/2563814.png"  alt="c"/> <p> <b>100% secure payments</b> <br/> We support Cards, Wallets, EMI and COD</p></div>
+    <div> <img src="https://cdn-icons-gif.flaticon.com/6172/6172532.gif"  alt="e"/> <p> <b>Vast service network</b> <br/> 1000 Mi service-centers across 600 cities</p></div>
         </div>
         <div className="PreFooter2">
              <div> <p>LET'S STAY IN TOUCH</p><span>Get updates on sales specials and more</span></div>
@@ -32,10 +32,10 @@ const Footer = ({footer}) => {
 
              <div className='images'>
                 {/* {facebookIcon} {youtubeIcon} {instagramIcon} {twitterIcon}  */}
-                <img src="https://cdn-icons-png.flaticon.com/128/3670/3670124.png"/>
-                <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"/>
-                <img src="https://cdn-icons-png.flaticon.com/128/733/733579.png"/>
-                <img src="https://cdn-icons-png.flaticon.com/128/2504/2504957.png"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/3670/3670124.png" alt="b"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png" alt="1"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/733/733579.png"  alt="2"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/2504/2504957.png"  alt="3"/>
              </div>
         </div>
 
